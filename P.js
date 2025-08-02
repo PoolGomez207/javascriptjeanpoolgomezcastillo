05b8391d526b463ebc96e4d9ee002c73e9af1353
KevinPogo <kevinpenagosieb@gmail.com>
2025-08-02 11:01:47 -0500

    subiendo archivos

---
+++ P.js
@@ -0,0 +1,10 @@
+var numero=13;
+console.log("el valor del numero:"+ numero);
+let nombre="jean pool gòmez castillo"
+var edad=17;
+var salario=5000;
+var hijos=2;
+console.log("mi nombre es:"+nombre+ " , mi edad es:" +edad+ ", mi sueldo es:" +salario+"y tendr dos hijos: "+hijos);
+let masculino=true;
+let femenino=false;
+console.log("mi genero es"+ "masculino:"+ masculino +"femenino:"+ femenino);
